6c3c9bedbf2ecd80aaec9c8fd2c99e67ec6e2d05
Gusev Roman <roman.gusev@advdigital.ru>
2017-07-14 17:49:48 +0300

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
+var path = require("path");
+
 var config = {
-   entry: './main.js',
-    
-   output: {
-      path:'./',
-      filename: 'index.js',
-   },
-    
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-    
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-                
-            query: {
-               presets: ['es2015', 'react']
+    // точка входа в сборку js
+    entry: "./main.js",
+    // точка выхода js
+    output: {
+        path: __dirname,
+        filename: "index.js"
+    },
+
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    // Зависимости
+                    presets: ['es2015', 'react']
+                }
             }
-         }
-      ]
-   }
+        ]
+    }
 }
 
 module.exports = config;
